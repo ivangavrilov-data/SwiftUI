6def869820afe320607db229adc57998af83abef
ivangavrilov-swift <ivangavrilove888@gmail.com>
2024-11-21 21:36:10 +0400

    Customise view properties

---
+++ Customise view with properties/Customise view with properties/Customise_view_with_propertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Customise_view_with_propertiesApp.swift
+//  Customise view with properties
+//
+//  Created by Иван Гаврилов on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Customise_view_with_propertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
