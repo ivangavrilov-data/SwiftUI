0a105be78fce2062c7837beb0a0e1ebe82109406
ivangavrilov-swift <ivangavrilove888@gmail.com>
2024-11-21 20:11:39 +0400

    Extra commit

--- LACWE Understanding test/LACWE Understanding test/ContentView.swift
+++ LACWE Understanding test/LACWE Understanding test/ContentView.swift
@@ -10,10 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(
+                    .system(
+                        size: 40,
+                        weight: .bold,
+                        design: .default
+                        )
+                    )
+            Text("Hello, world!")
+                .font(
+                    .system(size: 40,
+                            weight: .thin,
+                            design: .default
+                           
+                           )
+                )
+            Text("Hello, world!")
+                .font(.title2)
+            Text("Hello, world!")
+                .font(.title3)
+            Text("Hello, world!")
+                .font(.headline)
+            
+            Spacer()
+            
+            Text("Hello, world!")
+                .font(.body)
+            Text("Hello, world!")
+                .font(.callout)
+            Text("Hello, world!")
+                .font(.subheadline)
+            Text("Hello, world!")
+                .font(.footnote)
+            Text("Hello, world!")
+                .font(.caption)
+            Text("Hello, world!")
+                .font(.caption2)
         }
         .padding()
     }
