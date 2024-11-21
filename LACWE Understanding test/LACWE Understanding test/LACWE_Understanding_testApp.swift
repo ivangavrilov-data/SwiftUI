b7643fc83f44031a3cd4d0cf47465b0a62ef8562
ivangavrilov-swift <ivangavrilove888@gmail.com>
2024-11-21 20:09:38 +0400

    Understanding text

---
+++ LACWE Understanding test/LACWE Understanding test/LACWE_Understanding_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  LACWE_Understanding_testApp.swift
+//  LACWE Understanding test
+//
+//  Created by Иван Гаврилов on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LACWE_Understanding_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
