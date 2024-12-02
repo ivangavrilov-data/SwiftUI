4abff97b896b55a2eed22a49a001143470542cc8
ivangavrilov-swift <ivangavrilove888@gmail.com>
2024-12-02 11:01:13 +0400

    DynamicContent

---
+++ DynamicContent/DynamicContent/DynamicContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicContentApp.swift
+//  DynamicContent
+//
+//  Created by Иван Гаврилов on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
